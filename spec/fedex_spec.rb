a5fbdf82ada3841df66d4ecb19911021f54bfa1c
laserlemon <steve.richert@gmail.com>
2010-05-24 20:47:38 +0800

    Added a little ugliness to make sure the FedEx spec actually tests for the default options, rather than what is set in the spec_helper file.

--- spec/fedex_spec.rb
+++ spec/fedex_spec.rb
@@ -2,10 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "FedEx" do
   it "should return the default options" do
+    spec_helper_options = Shippinglogic::FedEx.options
+    Shippinglogic::FedEx.instance_variable_set("@options", nil)
+
     Shippinglogic::FedEx.options.should == {
-      :test => true,
+      :test => false,
       :production_url => "https://gateway.fedex.com:443/xml",
       :test_url => "https://gatewaybeta.fedex.com:443/xml"
     }
+
+    Shippinglogic::FedEx.instance_variable_set("@options", spec_helper_options)
   end
 end
