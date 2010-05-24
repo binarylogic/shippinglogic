2d16bd75727bd59b367a0bce40d9fb0b4e8e987b
laserlemon <steve.richert@gmail.com>
2010-05-24 21:03:57 +0800

    Built the basal UPS request module, including authentication building.

---
+++ lib/shippinglogic/ups/request.rb
@@ -0,0 +1,31 @@
+module Shippinglogic
+  class UPS
+    # Methods relating to building and sending a request to UPS's web services.
+    module Request
+      private
+        # Convenience method for sending requests to UPS
+        def request(body)
+          real_class.post(base.url, :body => body)
+        end
+        
+        # Convenience method to create a builder object so that our builder options are consistent across
+        # the various services.
+        #
+        # Ex: if I want to change the indent level to 3 it should change for all requests built.
+        def builder
+          b = Builder::XmlMarkup.new(:indent => 2)
+          b.instruct!
+          b
+        end
+        
+        # A convenience method for building the authentication block in your XML request
+        def build_authentication(b)
+          b.AccessRequest(:"xml:lang" => "en-US") do
+            b.AccessLicenseNumber base.key
+            b.UserId base.account
+            b.Password base.password
+          end
+        end
+    end
+  end
+end
