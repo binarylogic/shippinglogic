e17245cee63cde12b146bca1be2f697b7805dd3a
laserlemon <steve.richert@gmail.com>
2010-05-28 21:28:22 +0800

    Revamped the service-agnostic Attributes module spec.

--- spec/attributes_spec.rb
+++ spec/attributes_spec.rb
@@ -1,58 +1,61 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "FedEx Attributes" do
+describe "Service Attributes" do
+  before(:all) do
+    class Service
+      ATTRIBUTE_TYPES = %w(array integer float decimal boolean string text datetime)
+      
+      include Shippinglogic::Attributes
+      
+      ATTRIBUTE_TYPES.each do |type|
+        attribute :"#{type}_without_default",     type
+        attribute :"#{type}_with_default_value",  type, :default => 1
+        attribute :"#{type}_with_default_proc",   type, :default => lambda{ Time.now }
+      end
+    end
+  end
+  
+  after(:all) do
+    Object.send(:remove_const, :Service)
+  end
+  
   it "should allow setting attributes upon initialization" do
-    tracking = new_fedex.track(:tracking_number => fedex_tracking_number)
-    tracking.tracking_number.should == fedex_tracking_number
+    service = Service.new(:string_without_default => "TEST")
+    service.string_without_default.should == "TEST"
   end
   
   it "should allow setting attributes individually" do
-    tracking = new_fedex.track
-    tracking.tracking_number = fedex_tracking_number
-    tracking.tracking_number.should == fedex_tracking_number
+    service = Service.new
+    service.string_without_default = "TEST"
+    service.string_without_default.should == "TEST"
   end
   
   it "should allow setting attributes with a hash" do
-    tracking = new_fedex.track
-    tracking.attributes = {:tracking_number => fedex_tracking_number}
-    tracking.tracking_number.should == fedex_tracking_number
+    service = Service.new
+    service.attributes = {:string_without_default => "TEST"}
+    service.string_without_default.should == "TEST"
   end
   
   it "should allow reading attributes" do
-    tracking = new_fedex.track
-    tracking.attributes = {:tracking_number => fedex_tracking_number}
-    tracking.attributes.should == {:tracking_number => fedex_tracking_number}
+    service = Service.new
+    service.attributes = {:string_without_default => "TEST"}
+    service.attributes.should be_a Hash
+    service.attributes.should have_key :string_without_default
+    service.attributes[:string_without_default].should == "TEST"
   end
   
   it "should implement defaults" do
-    rates = new_fedex.rate
-    rates.shipper_residential.should == false
+    service = Service.new
+    service.integer_with_default_value.should == 1
   end
   
   it "should use blank array as defaults for arrays" do
-    rates = new_fedex.rate
-    rates.special_services_requested.should == []
+    service = Service.new
+    service.array_without_default.should == []
   end
   
   it "should call procs during run time if a default is a proc" do
-    rates = new_fedex.rate
-    rates.ship_time.to_s.should == Time.now.to_s
-  end
-  
-  it "should reset the cached response if an attribute changes" do
-    use_response(:rate_defaults)
-    
-    fedex = new_fedex
-    rates = fedex.rate
-    rates.attributes = fedex_shipper
-    rates.attributes = fedex_recipient
-    rates.attributes = fedex_package
-    rates.size.should == 6
-    
-    use_response(:rate_non_custom_packaging)
-    
-    rates.packaging_type = "FEDEX_ENVELOPE"
-    rates.package_weight = 0.1
-    rates.size.should == 5
+    service = Service.new
+    service.datetime_with_default_proc.to_s.should == Time.now.to_s
   end
 end
