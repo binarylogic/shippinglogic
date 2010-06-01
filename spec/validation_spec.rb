352737f7c4f11a082750968aaace2120173b3d95
laserlemon <steve.richert@gmail.com>
2010-06-01 09:07:54 +0800

    Revamped the validation spec, making it a bit more robust.

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,10 +1,46 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "FedEx Validation" do
-  it "should not be valid" do
-    use_response(:blank)
-    rates = new_fedex.rate
-    rates.valid?.should == false
-    rates.errors.should == ["The response from FedEx was blank."]
+class ServiceWithValidation
+  include Shippinglogic::Validation
+end
+
+describe "Service Validation" do
+  after(:all) do
+    Object.send(:remove_const, :ServiceWithValidation)
   end
+  
+  it "should provide an empty errors array" do
+    service = ServiceWithValidation.new
+    service.errors.should == []
+  end
+  
+  #context "when calling the valid? method" do
+    it "should call the target method" do
+      service = ServiceWithValidation.new
+      service.should_receive(:target)
+      service.valid?
+    end
+    
+    it "should return true if no errors are encountered" do
+      service = ServiceWithValidation.new
+      service.stub!(:target).and_return(nil)
+      service.valid?.should == true
+    end
+    
+    it "should not append any error messages if no errors are encountered" do
+      service = ServiceWithValidation.new
+      service.stub!(:target).and_return(nil)
+      service.valid?
+      service.errors.size.should == 0
+    end
+    
+    it "should rescue and append any Shippinglogic::Error messages" do
+      service = ServiceWithValidation.new
+      error = Shippinglogic::Error.new("ERROR")
+      service.stub!(:target).and_return{ raise error }
+      service.valid?.should_not raise_error(error.class)
+      service.errors.size.should == 1
+      service.errors.first.should == "ERROR"
+    end
+  #end
 end
