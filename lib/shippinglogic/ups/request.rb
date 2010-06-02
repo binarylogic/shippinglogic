f6056e7bcb805f4951888a34d8bd1b8b13c8a15b
laserlemon <steve.richert@gmail.com>
2010-06-02 11:38:17 +0800

    Added a method to DRY up address building in UPS API requests.

--- lib/shippinglogic/ups/request.rb
+++ lib/shippinglogic/ups/request.rb
@@ -28,6 +28,22 @@ module Shippinglogic
             b.Password base.password
           end
         end
+        
+        # A convenience method for building the address block in your XML request
+        def build_address(b, type)
+          address_lines = send("#{type}_streets").to_s.split(/(?:\s*\n\s*)+/m, 3)
+          
+          b.Address do
+            b.AddressLine1 address_lines[0] if address_lines[0]
+            b.AddressLine2 address_lines[1] if address_lines[1]
+            b.AddressLine3 address_lines[2] if address_lines[2]
+            b.City send("#{type}_city") if send("#{type}_city")
+            b.StateProvinceCode send("#{type}_state") if send("#{type}_state")
+            b.PostalCode send("#{type}_postal_code") if send("#{type}_postal_code")
+            b.CountryCode send("#{type}_country") if send("#{type}_country")
+            b.ResidentialAddressIndicator attribute_names.include?("#{type}_residential") && send("#{type}_residential")
+          end
+        end
     end
   end
 end
