d86c47367b6c03e5896fa79bee81328f4927d951
Aaron Davis <aaronzdavis@gmail.com>
2013-07-14 11:57:28 -0700

    remove Enumerations

--- lib/shippinglogic/ups/rate.rb
+++ lib/shippinglogic/ups/rate.rb
@@ -76,7 +76,7 @@ module Shippinglogic
     #   rates.first
     #   #<Shippinglogic::UPS::Rate::Service:0x10354d108 @currency="USD", @speed=nil,
     #     @rate=#<BigDecimal:10353ac10,'0.1885E2',18(18)>, @type="Ground", @name="Ground">
-    #   
+    #
     #   # to show accessor methods
     #   rates.first.name
     #   # => "Ground"
@@ -84,10 +84,10 @@ module Shippinglogic
       def self.path
         "/Rate"
       end
-      
+
       # Each rate result is an object of this class
       class Service; attr_accessor :name, :type, :speed, :rate, :currency; end
-      
+
       # shipper options
       attribute :shipper_name,                :string
       attribute :shipper_streets,             :string
@@ -95,7 +95,7 @@ module Shippinglogic
       attribute :shipper_state,               :string
       attribute :shipper_postal_code,         :string
       attribute :shipper_country,             :string,      :modifier => :country_code
-      
+
       # recipient options
       attribute :recipient_name,              :string
       attribute :recipient_streets,           :string
@@ -104,7 +104,7 @@ module Shippinglogic
       attribute :recipient_postal_code,       :string
       attribute :recipient_country,           :string,      :modifier => :country_code
       attribute :recipient_residential,       :boolean,     :default => false
-      
+
       # packaging options
       attribute :packaging_type,              :string,      :default => "00"
       attribute :package_count,               :integer,     :default => 1
@@ -114,84 +114,84 @@ module Shippinglogic
       attribute :package_width,               :float
       attribute :package_height,              :float
       attribute :package_dimension_units,     :string,      :default => "IN"
-      
+
       # monetary options
       attribute :currency_type,               :string
       attribute :insured_value,               :decimal
       attribute :payor_account_number,        :string,      :default => lambda { |shipment| shipment.base.account }
-      
+
       # delivery options
       attribute :service_type,                :string
       attribute :dropoff_type,                :string,      :default => "01"
       attribute :saturday,                    :boolean,     :default => false
-      
+
       # misc options
       attribute :documents_only,              :boolean,     :default => false
-      
+
       private
         def target
           @target ||= parse_response(request(build_request))
         end
-        
+
         def build_request
           b = builder
           build_authentication(b)
           b.instruct!
-          
+
           b.RatingServiceSelectionRequest do
             b.Request do
               b.RequestAction "Rate"
               b.RequestOption service_type ? "Rate" : "Shop"
             end
-            
+
             b.PickupType do
               b.Code dropoff_type
             end
-            
+
             b.Shipment do
               b.Shipper do
                 b.Name shipper_name
                 b.ShipperNumber payor_account_number
                 build_address(b, :shipper)
               end
-              
+
               b.ShipTo do
                 b.CompanyName recipient_name
                 build_address(b, :recipient)
               end
-              
+
               if service_type
                 b.Service do
                   b.Code service_type
                 end
               end
-              
+
               b.DocumentsOnly if documents_only
-              
+
               package_count.times do |i|
                 b.Package do
                   b.PackagingType do
                     b.Code packaging_type
                   end
-                  
+
                   b.Dimensions do
                     b.UnitOfMeasurement do
                       b.Code package_dimension_units
                     end
-                    
+
                     b.Length "%.2f" % package_length
                     b.Width "%.2f" % package_width
                     b.Height "%.2f" % package_height
                   end
-                  
+
                   b.PackageWeight do
                     b.UnitOfMeasurement do
                       b.Code package_weight_units
                     end
-                    
+
                     b.Weight "%.1f" % package_weight
                   end
-                  
+
                   b.PackageServiceOptions do
                     if insured_value
                       b.InsuredValue do
@@ -202,20 +202,21 @@ module Shippinglogic
                   end
                 end
               end
-              
+
               b.ShipmentServiceOptions do
                 b.SaturdayDelivery if saturday
               end
             end
           end
         end
-        
+
         def parse_response(response)
           return [] if !response[:rated_shipment]
-          
+
           response[:rated_shipment].collect do |details|
             service = Service.new
-            service.name = Enumerations::SERVICE_TYPES[details[:service][:code]]
+            # service.name = Enumerations::SERVICE_TYPES[details[:service][:code]]
+            service.name = details[:service][:code]
             service.type = service.name
             service.speed = (days = details[:guaranteed_days_to_delivery]) && (days.to_i * 86400)
             service.rate = BigDecimal.new(details[:total_charges][:monetary_value])
